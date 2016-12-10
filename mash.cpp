235247611719c1f9635ae368e489ae78e7314041
MikelSV <mikelsv@msn.com>
2016-12-10 04:04:03 +0300

    Release

---
+++ mash.cpp
@@ -0,0 +1,96 @@
+#define PROJECTNAME "mash"
+#define PROJECTVER PROJECTNAME ## _versions
+#define USEMSV_GENERALCPP
+
+#define USEMSV_HASH
+#define USEMSV_MSL_FL
+#define USEMSV_PCRE
+
+class msl_fl_extfunc;
+msl_fl_extfunc *msl_fl_extfunc_f = 0;
+
+#define MSL_FL_EXTFUNC_DEFAULT msl_fl_extfunc_f
+
+#include "msvcore2/msvcore.cpp"
+
+Versions PROJECTVER[]={
+	// new version to up
+	"0.0.0.1", "08.12.2016 14:39"
+};
+
+#include "mash.h"
+
+class MashPoint{
+public:
+	MString point;
+};
+
+int main(int args, char* arg[]){
+	msvcoremain(args, arg);
+	print(PROJECTNAME, " v.", PROJECTVER[0].ver, " (", PROJECTVER[0].date, ").\r\n");
+
+	mash_msl_fl_extfunc mash_extfunc;
+	msl_fl_extfunc_f = &mash_extfunc;
+
+	OList<MashPoint> points;
+	msl_fl msl;
+	int ret = 0;
+
+
+	//// match
+	//pcre2_match_data *match_data = pcre2_match_data_create(100, NULL);
+	//size_t *ovector = pcre2_get_ovector_pointer(match_data);
+	//int perrcode; size_t erroffset;
+
+	//VString rule = ".*.sdf";
+	//VString line = "ololo.sdf";
+
+	//pcre2_code *p = pcre2_compile(rule, rule, 0, &perrcode, &erroffset, 0);
+
+	//int f = pcre2_match(p, line, line, 0, 0, match_data, 0);
+
+	//unsigned char buf[S1K];
+	//pcre2_get_error_message(perrcode, buf, S1K);
+	//// End test
+
+	// Get args
+	ILink &link = msvcorestate.link;
+
+	// Get first option
+	VString path = link.GetArg();
+
+	if(!path){
+		print("Please set file name.\r\n");
+		return -1;
+	}
+
+	ILink mlink(path);
+	chdir(SString(mlink.GetProtoDomainPath()));
+
+	// Args
+	msl_value *val = msl.SetValue("argv", "");
+
+	for(int i = 0; i < args; i ++)
+		val->Set(itos(i), arg[i]);
+
+	// Run
+	msl.DoFile(mlink.file);
+	print(msl.GetOutput());
+
+	// Get points
+	while(VString point = link.GetArg()){
+		MashPoint * poi = points.NewE();
+		poi->point = point;
+	}
+
+	MashPoint * poi = 0;
+	while(poi = points.Next(poi)){
+		//DoMash();
+	}
+
+	if(!ret){
+		//print("Error. Exit.\r\n");
+	}
+
+	return !ret;
+}
---
+++ mash.h
@@ -0,0 +1,190 @@
+MString MashFilesIgnore(VString files, VString ignore);
+MString MashFilesSelect(VString files, VString reg);
+MString MashFilesDel(VString files, VString ignore);
+
+class mash_msl_fl_extfunc : public msl_fl_extfunc{
+public:
+	virtual int DoCodeFunctionExec(VString name, msl_fl_fargs &args, msl_value &val){
+		// exec
+		//if(name=="print" || name=="echo"){
+		//	//for(int i=0; i<args.Sz(); i++){
+		//		//SetOutput(args[i].val.val);
+		//		//print(args[i].val.val);
+		//	//}
+		//	//return 1;
+		//}
+
+		if(name == "mash_files_ignore" && args.Sz() == 2){
+			val.val = MashFilesIgnore(args[0].val.val, args[1].val.val);
+			return 1;
+		}
+
+		if(name == "mash_files_select" && args.Sz() == 2){
+			val.val = MashFilesSelect(args[0].val.val, args[1].val.val);
+			return 1;
+		}
+
+		if(name == "mash_files_del" && args.Sz() == 2){
+			val.val = MashFilesDel(args[0].val.val, args[1].val.val);
+			return 1;
+		}
+
+		if(name == "testextfunc"){
+			val.val="ok";
+			return 1;
+		}
+
+		return 0;
+	}
+};
+
+
+MString MashFilesIgnore(VString files, VString ignore){
+	IList <VString> list;
+
+	SString tmp;
+	ignore = tmp = Replace(ignore, "*", ".*");	
+	
+	while(files){
+		VString file = PartLine(files, files,  "\r\n");
+
+		list.n() = file;
+		list.Added();
+	}
+
+	// match
+	pcre2_match_data *match_data = pcre2_match_data_create(100, NULL);
+	size_t *ovector = pcre2_get_ovector_pointer(match_data);
+	int perrcode; size_t erroffset;
+
+	while(ignore){
+		VString rule = PartLine(ignore, ignore,  "\r\n");
+
+		if(!rule || rule[0] == '#')
+			continue;
+
+		pcre2_code *p = pcre2_compile(rule, rule, 0, &perrcode, &erroffset, 0);
+
+		for(unsigned int i = 0; i < list.Size(); i ++){
+			int f = pcre2_match(p, list[i], list[i], 0, 0, match_data, 0);
+
+			if(f > 0){
+				//print("Del: ", list[i], ". ", rule, "\r\n");
+				list.Del(i);
+				i --;
+			}
+
+			if(perrcode != 100 && f < 0)
+				print("Pcre2 error!\r\n");
+		}
+
+		pcre2_code_free(p);
+	}
+
+	pcre2_match_data_free(match_data);
+
+	LString ls;
+	for(unsigned int i = 0; i < list.Size(); i ++){
+		if(i)
+			ls + "\r\n";
+		ls + list[i];
+	}
+
+	return (VString)ls;
+}
+
+class VStringInt{
+public:
+	VString key;
+	int val;
+};
+
+
+MString MashFilesSelect(VString files, VString reg){
+	IList<VStringInt> list;
+
+	SString tmp;
+	reg = tmp = Replace(reg, "*", ".*");	
+	
+	while(files){
+		VString file = PartLine(files, files,  "\r\n");
+
+		list.n().key = file;
+		list.n().val = 0;
+		list.Added();
+	}
+
+	// match
+	pcre2_match_data *match_data = pcre2_match_data_create(100, NULL);
+	size_t *ovector = pcre2_get_ovector_pointer(match_data);
+	int perrcode; size_t erroffset;
+
+	while(reg){
+		VString rule = PartLine(reg, reg, "\r\n");
+
+		if(!rule || rule[0] == '#')
+			continue;
+
+		pcre2_code *p = pcre2_compile(rule, rule, 0, &perrcode, &erroffset, 0);
+
+		for(unsigned int i = 0; i < list.Size(); i ++){
+			int f = pcre2_match(p, list[i].key, list[i].key, 0, 0, match_data, 0);
+
+			if(f > 0){
+				list[i].val = 1;
+			}
+
+			if(perrcode != 100 && f < 0)
+				print("Pcre2 error!\r\n");
+		}
+
+		pcre2_code_free(p);
+	}
+
+	pcre2_match_data_free(match_data);
+
+	LString ls;
+	for(unsigned int i = 0; i < list.Size(); i ++){
+		if(!list[i].val)
+			continue;
+
+		if(ls)
+			ls + "\r\n";
+		ls + list[i].key;
+	}
+
+	return (VString)ls;
+}
+
+
+
+MString MashFilesDel(VString files, VString ignore){
+	IList <VString> list;
+	
+	while(files){
+		VString file = PartLine(files, files,  "\r\n");
+
+		list.n() = file;
+		list.Added();
+	}
+
+	while(ignore){
+		VString rule = PartLine(ignore, ignore,  "\r\n");
+
+		for(unsigned int i = 0; i < list.Size(); i ++){
+			if(list[i] == rule){
+				list.Del(i);
+				i --;
+			}
+		}
+	}
+
+	LString ls;
+	for(unsigned int i = 0; i < list.Size(); i ++){
+		if(i)
+			ls + "\r\n";
+		ls + list[i];
+	}
+
+	return (VString)ls;
+}
